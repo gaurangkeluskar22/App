834e4d6b3b65dd375f6d0fa3a1f617e5e112e318
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2024-03-27 01:22:17 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 require('dotenv').config()
 const authRouter = require('./routes/authRouter')
@@ -8,13 +9,18 @@ const port = process.env.SERVER_PORT
 const cors = require('cors')
 const { app, server } = require('./socket/socket')
 
+const dirname = path.resolve()
 app.use(express.json())
 app.use(cors())
 
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 app.use('/api/message', messageRouter)
+app.use(express.static(path.join(dirname, "build")))
 
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(dirname, "build", "index.html"))
+})
 
 
 server.listen(port, ()=>{
