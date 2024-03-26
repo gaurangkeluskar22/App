631faa6b0a19c60c594cf5f393e01c13a10c79c0
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2024-03-27 03:17:12 +0530

    changes

--- src/Context/SocketContext.js
+++ src/Context/SocketContext.js
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
     useEffect(()=>{
         if(userId){
             //https://chatapp-3rqf.onrender.com
-            const socket = io("https://chatapp-3rqf.onrender.com:9999",{
+            const socket = io("https://chatapp-3rqf.onrender.com",{
                 query:{
                     userId : userId
                 }
