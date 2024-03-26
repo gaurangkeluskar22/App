0a478f63c0e228738ee5398c8a9b9416540c1644
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2024-03-27 01:55:21 +0530

    made changes

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors:{
-        origin : ['http://localhost:3000'],
+        origin : ['http://localhost:9999', 'https://chatapp-3rqf.onrender.com/'],
         methods : ["GET","POST"]
     }
 })
@@ -30,6 +30,8 @@ io.on("connection", (socket)=>{
     if(userId !== "undefined"){
         userSocketMap[userId] = socket.id
     }
+    
+    console.log(userSocketMap)
 
     // io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap))
